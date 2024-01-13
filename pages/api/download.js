1ef4b801e3bcd5430419b173e086ff0d5e42f21e
Caleb Harris <caleb@Calebs-Air.lan>
2024-01-13 01:43:29 -0500

    file named changed

--- pages/api/download.js
+++ pages/api/download.js
@@ -1,17 +1,20 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
 export default function handler(req, res) {
   // Replace 'path/to/your/file.txt' with the actual path to your file
-  const filePath = path.join(process.cwd(), 'public/TechResume.pdf');
+  const filePath = path.join(process.cwd(), "public/CalebHarris.pdf");
 
   // Read the file
   const fileContents = fs.readFileSync(filePath);
 
   // Set the response headers
-  res.setHeader('Content-Disposition', 'attachment; filename="CalebHarris.pdf"');
-  res.setHeader('Content-Type', 'application/octet-stream');
-  res.setHeader('Content-Length', fileContents.length);
+  res.setHeader(
+    "Content-Disposition",
+    'attachment; filename="CalebHarris.pdf"'
+  );
+  res.setHeader("Content-Type", "application/octet-stream");
+  res.setHeader("Content-Length", fileContents.length);
 
   // Send the file as the response
   res.send(fileContents);
